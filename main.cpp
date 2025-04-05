0ef0d9c93e6b020c56dd9591cf3b5f667519c283
RickySaidHi <rardisana4@gmail.com>
2025-04-05 13:27:26 -0500

    FIXED

--- main.cpp
+++ main.cpp
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-    cout << "Hello Word" << endl;
-    cout << "YO YO YO" << endl;
-=======
 #include <iostream>
 
 using namespace std;
@@ -17,5 +7,4 @@ int main()
     cout << "Hello Word" << endl;
     cout << "YO YO YO" << endl;
     cout << "YO YO YO" << endl;
->>>>>>> 568afe1d360c5470f673bf5b60953ecb6e549f06
 }
