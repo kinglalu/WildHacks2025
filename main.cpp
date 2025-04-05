b747e56a85228e1ed3ac2e517898fe378262e815
RamenNoodles1129 <ramonvazquezjr1129@gmail.com>
2025-04-05 14:55:39 -0500

    Made te clear input buffer and the get user choice

--- main.cpp
+++ main.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <limits>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello Word" << endl;
-    cout << "YO YO YO" << endl;
-    cout << "YO YO YO" << endl;
-    cout << "AM I HERE" << endl;
-    cout << "Parth do something!!!" << endl;
+// Utility functions for input validation
+void clearInputBuffer() {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+}
+
+int getUserChoice(int min, int max) {
+    int choice;
+    while (true) {
+        cout << "Enter your choice (" << min << "-" << max << "): ";
+        if (cin >> choice && choice >= min && choice <= max) {
+            clearInputBuffer();
+            return choice;
+        } else {
+            cout << "Invalid input. Please try again." << endl;
+            clearInputBuffer();
+        }
+    }
 }
